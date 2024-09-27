efc3ebbdb0941743d1f1651bbcb1128acb2b18a9
Martin Loic <loic.martin2000@gmail.com>
2024-09-27 08:23:21 +0200

    relation between employers and jobs listing + tag

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'jobs_listing';
     protected $fillable = ['title', 'salary'];
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
