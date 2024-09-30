4033af24b1a8315c205c36ab3d1af135d7eb0b33
Martin Loic <loic.martin2000@gmail.com>
2024-09-30 08:16:44 +0200

    from to update or delete a job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,7 @@
 <x-slot:heading>
         About me
     </x-slot:heading>
-                <h2> {{$job['title']}} </h2>
-                <p>The position <strong> {{$job['title']}} </strong> pays {{$job['salary']}} per month</p>
+                <h2> {{$job->title}} </h2>
+                <p>The position <strong> {{$job->title}} </strong> pays {{$job['salary']}} per month</p>
+                <x-button class="mt-6" href="{{$job->id}}/edit">Edit this Job</a></x-button>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,41 +4,80 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 use App\Models\Job;
 
+//page principale
 Route::get('/', function () {
     return view('home');
 });
 
+//Index des jobs
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(5);
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+//Creer un job
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Voir un job
 Route::get('/jobs/{id}', function ($id) {
-        $job = Job::find($id);
-        if(!$job) {
-            abort(404);
-        }
+    $job = Job::find($id);
+    if (!$job) {
+        abort(404);
+    }
     return view('jobs.show', data: ['job' => $job]);
 });
 
+//Nous contacter
 Route::get('/contact', function () {
     return view('contact');
 });
 
+//Form pour créer un job
 Route::post('/jobs', function () {
+    //Validation
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required']
     ]);
 
+    //Créer le job
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
         'employer_id' => 5
     ]);
     return redirect('/jobs');
+});
+
+//Update un job
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('jobs/' . $job->id);
+});
+
+//Delete un job
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+    $job->delete();
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    if (!$job) {
+        abort(404);
+    }
+    return view('jobs.edit', data: ['job' => $job]);
 });
